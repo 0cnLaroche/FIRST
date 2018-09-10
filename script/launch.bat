dbbc62af0a95e98ccd060d7f4f6c47bcad0f7315
Samuel Laroche <samuel.laroche@hrsdc-rhdcc.gc.ca>
2018-09-10 10:49:19 -0400

    add scripts

---
+++ script/launch.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar "\\hrdc-drhc.net\nc_common-commun$\IITB-DGIIT\SPAM-SPAG\RM-GR\MACTI-ITCAM\Restricted\Time Reporting Tools Maintenance\SAP-CATS Administration\Database\FIRST_2.jar"
