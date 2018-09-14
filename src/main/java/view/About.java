7833ec194b7a5645534902e7323a4ba8484b441d
Samuel <samuellaroche@live.ca>
2018-09-14 08:48:25 -0400

    fixed URL in About

--- src/main/java/view/About.java
+++ src/main/java/view/About.java
@@ -11,28 +11,28 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebView;
 
 public class About extends BorderPane {
-	
+
 	private WebView guide;
-	
+
 	public About(App main) {
 		guide = new WebView();
 		String html = new String();
-		
-		InputStream is = About.class.getResourceAsStream("/res/guide.html");
-		
+
+		InputStream is = About.class.getResourceAsStream("/guide.html");
+
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"))){
 			String line;
 			while ((line = reader.readLine()) != null){
 				html += line;
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			System.err.println("File not Found");
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		guide.getEngine().loadContent(html);
 		this.setCenter(guide);
 	}
